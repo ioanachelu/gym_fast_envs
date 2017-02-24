90a85e9c0b26c4ae796b3ab21f45a5a29b05eef5
ioanachelu <ioana.chelu28@gmail.com>
2017-02-24 12:47:35 +0200

    Started working on ga3c

--- gym_fast_envs/catcher.py
+++ gym_fast_envs/catcher.py
@@ -277,19 +277,19 @@ if __name__ == '__main__':
     game.set_seed(23)  # test change of seed
 
     start = time.time()
-    o, t, r = game.reset()
+    o, t, r, i = game.reset()
     ep = 0
     step = 0
     tot_rw = 0
 
-    while ep <= 10:
-        o, t, r = game.step(game.actions[player_rng.choice(3)])
+    while True:
+        o, t, r, i = game.step(game.actions[player_rng.choice(3)])
         step += 1
         game.display()
         tot_rw += r
         if t:
             ep += 1
-            o, t, r = game.reset()
+            o, t, r, i = game.reset()
 
     print("Finished %d episodes in %d steps in %.2f. Total reward: %d.",
           (ep, step, time.time() - start, tot_rw))
